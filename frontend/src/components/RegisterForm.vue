e01b857232b4d2913d5117532bf703a76ee60cd1
Tereza Dohnalova <tddohnalova@gmail.com>
2022-04-10 12:40:20 +0200

    feat: add register form

---
+++ frontend/src/components/RegisterForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit="registerUser">
+    <label for="firstname">Firstname</label>
+    <input type="text" v-model="user.firstname" id="firstname" required />
+    <br />
+    <label for="lastname">Lastname</label>
+    <input type="text" v-model="user.lastname" id="lastname" required />
+    <br />
+    <label for="nickname">Nickname</label>
+    <input type="text" v-model="user.nickname" id="nickname" required />
+    <br />
+    <label for="email">E-mail</label>
+    <input type="email" v-model="user.email" id="email" />
+    <br />
+    <label for="password">Password</label>
+    <input type="password" v-model="user.password" id="password" required />
+    <br />
+    <label for="passwordConf">Password confirmation</label>
+    <input
+      type="password"
+      v-model="user.passwordConf"
+      id="passwordConf"
+      required
+    />
+    <br />
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "RegisterForm",
+  data() {
+    return {
+      user: {
+        firstname: "",
+        lastname: "",
+        nickname: "",
+        email: "",
+        password: "",
+        passwordConf: "",
+      },
+    };
+  },
+  methods: {
+    registerUser(event: SubmitEvent) {
+      event.preventDefault();
+      console.log(this.user);
+    },
+  },
+});
+</script>
--- frontend/src/views/RegisterUserView.vue
+++ frontend/src/views/RegisterUserView.vue
@@ -1,11 +1,14 @@
 <template>
   <div>Register User View</div>
+  <RegisterForm />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import RegisterForm from "@/components/RegisterForm.vue";
 
 export default defineComponent({
+  components: { RegisterForm },
   name: "RegisterUserView",
 });
 </script>
